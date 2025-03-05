fad80c3741862a34fdb9bfffd4a31f2467002768
2103pub <96341333+2103pub@users.noreply.github.com>
2025-03-06 06:28:59 +0900

    Add files via upload

--- glp1/js/script.js
+++ glp1/js/script.js
@@ -99,3 +99,23 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+$(function () {
+    // ウィンドウをスクロールしたら…
+    $(window).scroll(function () {
+        // ウィンドウの高さを取得
+        const wHeight = $(window).height();
+        // スクロールした量を取得
+        const wScroll = $(window).scrollTop();
+            // それぞれのblockクラスに対して…
+            $(".block").each(function () {
+                // それぞれのblockクラスのウィンドウからの高さを取得
+                const bPosition = $(this).offset().top;
+                // スクロールした量が要素の高さを上回ったら
+                // その数値にウィンドウの高さを引き、最後に200pxを足す
+            if (wScroll > bPosition - wHeight + 200) {
+                $(this).addClass("fadeIn");
+            }
+        });
+    });
+});
