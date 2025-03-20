572847236275d6829142522acbb7c127d95bded8
ka-ko1995 <kananana004@gmail.com>
2025-03-20 17:52:39 +0900

    Add files via upload

---
+++ glp1/common.js
@@ -0,0 +1,64 @@
+// JavaScript Document
+$(document).ready(function() {
+	
+	// Smooth scroll
+	var headerHeight = $('.header').height();
+	$('a[href^="#"]').click(function(){
+		var speed = 500;
+		var href= $(this).attr("href");
+		var target = $(href == "#" || href == "" ? 'html' : href);
+		var position = "";
+		position = target.offset().top - headerHeight;
+		$("html, body").animate({scrollTop:position}, speed, "swing");
+		return false;
+	});
+	
+	// SP navigation
+	$('.header__nav-button').children('button').click(function() {
+		$(this).toggleClass('is-open');
+		
+		if ($(this).hasClass('is-open')) {
+			$('.header__nav').addClass('is-active');
+		} else {
+			$('.header__nav').removeClass('is-active');
+		}	
+	});
+	$('.nav-item-1 a,.nav-item-2 a,.nav-item-3 a,.nav-item-4 a,.nav-item-5 a,.nav-item-6 a,.nav-item-7 a,.nav-item-8 a,.nav-item-9 a').click(function(){
+		$('.header__nav').removeClass('is-active');
+		$('button').removeClass('is-open');
+
+	});
+
+	
+	// Accordion
+	$('.faq__list-item .head').click(function(){
+		$(this).toggleClass('is-active');
+		$(this).next().toggleClass('is-active')
+	});
+	
+	// Swiper
+	var swiper01 = new Swiper('#swiper01', {
+		slidesPerView: 1,
+		loop: true,
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+	});
+	
+	var scrollStart = 100;
+	var distance = 0;
+	$(window).scroll(function() {
+		distance = $(this).scrollTop();
+		if(distance > scrollStart) {
+			$('.header').addClass('is-blur');
+		} else {
+			$('.header').removeClass('is-blur');
+		}
+	});
+	
+});
